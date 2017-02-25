1d540f4c54ca2d4bf1d5d5bb2f61c901249e8b84
Zongjian He <omale@outlook.com>
2017-02-25 08:33:10 +0800

    finished pitch perfect

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,3 +1,4 @@
+package com.example.android.background.sync;
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -14,10 +15,31 @@
  * limitations under the License.
  */
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+// DONE (9) Create WaterReminderIntentService and extend it from IntentService
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+//  DONE (10) Create a default constructor that calls super with the name of this class
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+//  DONE (11) Override onHandleIntent
+//      DONE (12) Get the action from the Intent that started this Service
+//      DONE (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class WaterReminderIntentService extends IntentService {
+
+    public WaterReminderIntentService() {
+        super(WaterReminderIntentService.class.getName());
+        Log.i("TAG", "WaterReminderIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i("TAG", "onHandleIntent");
+        String action = intent.getAction();
+        ReminderTasks.executeTask(getApplicationContext(), action);
+
+    }
+}
--- Lesson10-Hydration-Reminder/T10.02-Exercise-CreateNotification/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ Lesson10-Hydration-Reminder/T10.02-Exercise-CreateNotification/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -1,15 +1,29 @@
 package com.example.android.background.utilities;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
+
+import com.example.android.background.MainActivity;
+import com.example.android.background.R;
+
 /**
  * Utility class for creating hydration notifications
  */
 public class NotificationUtils {
 
-    // TODO (7) Create a method called remindUserBecauseCharging which takes a Context.
+    public static final int REQUEST_CODE = 1334;
+
+    // DONE (7) Create a method called remindUserBecauseCharging which takes a Context.
     // This method will create a notification for charging. It might be helpful
     // to take a look at this guide to see an example of what the code in this method will look like:
     // https://developer.android.com/training/notify-user/build-notification.html
-        // TODO (8) In the remindUser method use NotificationCompat.Builder to create a notification
+        // DONE (8) In the remindUser method use NotificationCompat.Builder to create a notification
         // that:
         // - has a color of R.colorPrimary - use ContextCompat.getColor to get a compatible color
         // - has ic_drink_notification as the small icon
@@ -20,19 +34,36 @@ public class NotificationUtils {
         // - sets the notification defaults to vibrate
         // - uses the content intent returned by the contentIntent helper method for the contentIntent
         // - automatically cancels the notification when the notification is clicked
-        // TODO (9) If the build version is greater than JELLY_BEAN, set the notification's priority
+        // DONE (9) If the build version is greater than JELLY_BEAN, set the notification's priority
         // to PRIORITY_HIGH.
-        // TODO (11) Get a NotificationManager, using context.getSystemService(Context.NOTIFICATION_SERVICE);
-        // TODO (12) Trigger the notification by calling notify on the NotificationManager.
+        // DONE (11) Get a NotificationManager, using context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // DONE (12) Trigger the notification by calling notify on the NotificationManager.
         // Pass in a unique ID of your choosing for the notification and notificationBuilder.build()
 
 
+    public void remindUserBecauseCharging (Context context) {
 
-    // TODO (1) Create a helper method called contentIntent with a single parameter for a Context. It
+        NotificationCompat.Builder aBuilder = new NotificationCompat.Builder(context);
+        aBuilder.setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_drink_notification)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle(context.getString(R.string.charging_reminder_notification_title))
+                .setContentText(context.getString(R.string.charging_reminder_notification_body))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("text"))
+                .setAutoCancel(true);
+
+        NotificationManager mgr = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mgr.notify(123, aBuilder.build());
+    }
+
+
+
+
+    // DONE (1) Create a helper method called contentIntent with a single parameter for a Context. It
     // should return a PendingIntent. This method will create the pending intent which will trigger when
     // the notification is pressed. This pending intent should open up the MainActivity.
-        // TODO (2) Create an intent that opens up the MainActivity
-        // TODO (3) Create a PendingIntent using getActivity that:
+        // DONE (2) Create an intent that opens up the MainActivity
+        // DONE (3) Create a PendingIntent using getActivity that:
             // - Take the context passed in as a parameter
             // - Takes an unique integer ID for the pending intent (you can create a constant for
             //   this integer above
@@ -41,12 +72,22 @@ public class NotificationUtils {
             // - Has the flag FLAG_UPDATE_CURRENT, so that if the intent is created again, keep the
             // intent but update the data
 
+    PendingIntent contentIntent(Context context) {
+        Intent it  = new Intent(context, MainActivity.class);
+        Intent[] its = {it};
+        return PendingIntent.getActivities(context, REQUEST_CODE, its, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
 
-    // TODO (4) Create a helper method called largeIcon which takes in a Context as a parameter and
+
+    // DONE (4) Create a helper method called largeIcon which takes in a Context as a parameter and
     // returns a Bitmap. This method is necessary to decode a bitmap needed for the notification.
-        // TODO (5) Get a Resources object from the context.
-        // TODO (6) Create and return a bitmap using BitmapFactory.decodeResource, passing in the
+        // DONE (5) Get a Resources object from the context.
+        // DONE (6) Create and return a bitmap using BitmapFactory.decodeResource, passing in the
         // resources object and R.drawable.ic_local_drink_black_24px
+
+    public Bitmap largeIcon(Context context) {
+        return BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_local_drink_black_24px);
+    }
 
 
 }
